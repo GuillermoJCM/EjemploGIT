81435b1c9e31bd4163158f196c90214c12476870
Guille <Guille@WIN-DU5CALUANT3>
2023-12-12 17:42:03 +0100

    Cambios en principal en becario

--- PRUEBA_GIT/src/main/java/principal/principal.java
+++ PRUEBA_GIT/src/main/java/principal/principal.java
@@ -11,6 +11,7 @@ public class principal {
 		System.out.println("Version Github");
 		System.out.println("Version editada el domingo");
 		System.out.println("Prueba pa unas cosas");
+		System.out.println("Soy becario y me dejan cambiar la clase principal");
 		
 	}
 
