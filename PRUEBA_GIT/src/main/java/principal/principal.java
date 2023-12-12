7a7606c8783d886271b3a3ce849ba284f883e7d4
Guille <Guille@WIN-DU5CALUANT3>
2023-12-12 17:52:22 +0100

    Ultimo push becario

--- PRUEBA_GIT/src/main/java/principal/principal.java
+++ PRUEBA_GIT/src/main/java/principal/principal.java
@@ -12,7 +12,7 @@ public class principal {
 		System.out.println("Version editada el domingo");
 		System.out.println("Prueba pa unas cosas");
 		System.out.println("Soy becario y me dejan cambiar la clase principal");
-		
+		System.out.println("ultima actualizacion para push");
 	}
 
 }
